59e3fe5999fac4ee6cf41aa83386df87d7668ed5
Amiron <Amiron49@gmail.com>
2021-03-28 16:06:01 +0200

    Feat(Extension): Example Extension

---
+++ Aris.Moe.OverlayTranslate.Extension/README.md
@@ -0,0 +1,27 @@
+# Snow Chrome Extension
+
+A small extension for making the browser a bit more christmassy ❄️
+
+![2020-10-22 18 18 26](https://user-images.githubusercontent.com/31168035/96901117-21845200-1493-11eb-8af8-32972b1efe04.gif)
+
+## Installation
+
+Navigate to the project directory and install the dependencies.
+
+```
+$ npm install
+```
+
+To build the extension, and rebuild it when the files are changed, run
+
+```
+$ npm start
+```
+
+After the project has been built, a directory named `dist` has been created. You have to add this directory to your Chrome browser:
+
+1. Open Chrome.
+2. Navigate to `chrome://extensions`.
+3. Enable _Developer mode_.
+4. Click _Load unpacked_.
+5. Select the `dist` directory.
---
+++ Aris.Moe.OverlayTranslate.Extension/src/background.ts
@@ -0,0 +1,43 @@
+import { MessageTypes } from "./types";
+
+const sendSnowStatus = (snowing: boolean) => {
+  const message = { type: "SNOW_STATUS", snowing };
+
+  // send message to popup
+  chrome.runtime.sendMessage(message);
+
+  // send message to every active tab
+  chrome.tabs.query({}, (tabs) => {
+    tabs.forEach((tab) => {
+      if (tab.id) {
+        chrome.tabs.sendMessage(tab.id, message);
+      }
+    });
+  });
+};
+
+let snowing = false;
+
+// Get locally stored value
+chrome.storage.local.get("snowing", (res) => {
+  if (res["snowing"]) {
+    snowing = true;
+  } else {
+    snowing = false;
+  }
+});
+
+chrome.runtime.onMessage.addListener((message: MessageTypes) => {
+  switch (message.type) {
+    case "REQ_SNOW_STATUS":
+      sendSnowStatus(snowing);
+      break;
+    case "TOGGLE_SNOW":
+      snowing = message.snowing;
+      chrome.storage.local.set({ snowing: snowing });
+      sendSnowStatus(snowing);
+      break;
+    default:
+      break;
+  }
+});
